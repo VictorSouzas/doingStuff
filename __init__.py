ce0d468529408e8289e7994a7af83e7b80b0fdf4
Victor <victor@localhost.localdomain>
2019-01-01 19:15:17 -0500

    hello, world!

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from flask import abort, Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hellp_world():
+    return 'Hello, world!'
+
+@app.route('/not_found', methods=['GET',])
+def not_found():
+    abort(401)
+
+if __name__ == '__main__':
+    args = {
+        'host': '127.0.0.1', 'port': 8080,
+        'debug': True, 'load_dotenv': True
+    }
+    app.run(**args)
